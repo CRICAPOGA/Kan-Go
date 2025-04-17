7644ab004af58de634cec834e7973b2e0f694b4f
MarianaG22 <142903466+MarianaG22@users.noreply.github.com>
2025-04-17 11:50:23 -0500

    fix - nombres a español

--- KanAndGo/Autenticacion/urls.py
+++ KanAndGo/Autenticacion/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login_register/', views.access, name='access'),
+    path('acceso/', views.acceso, name='acceso'),
 ]
--- KanAndGo/Autenticacion/views.py
+++ KanAndGo/Autenticacion/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-def access(request):
-    return render(request, 'login_register.html')
+def acceso(request):
+    return render(request, 'acceso.html')
