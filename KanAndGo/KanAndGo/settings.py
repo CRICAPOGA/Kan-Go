c4af4022b6e544bba3b27918fc8682a70b985093
CRICAPOGA <camilochoposada2@gmail.com>
2025-04-22 10:19:06 -0500

    refactor - Actualizar configuración de hosting en settings.py

--- KanAndGo/KanAndGo/settings.py
+++ KanAndGo/KanAndGo/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-gi6%%5*t_^^)yx%atb@-d^(=)(h1qks^6rg8v#_eoikxb#zh+l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.ngrok-free.app",
+]
 
 
 # Application definition
