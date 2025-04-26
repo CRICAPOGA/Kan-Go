40fb872fad254ddc6d58b21f8770e69fab788dcb
MarianaG22 <142903466+MarianaG22@users.noreply.github.com>
2025-04-26 17:32:53 -0500

    refactor - Configuración para reportes

--- KanAndGo/KanAndGo/views.py
+++ KanAndGo/KanAndGo/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-
-def reportes(request):
-    return render(request, 'reportes.html')
